9d5fdb23823c83bb84802915ab054a38f6d9ec8f
theo <theo.francois.marechal@gmail.com>
2022-03-04 21:29:02 +0100

    adding cadvisor 4

--- Frontend/app.py
+++ Frontend/app.py
@@ -15,4 +15,4 @@ def get_sentence():
     return jsonify(statistics.text)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=5001)
